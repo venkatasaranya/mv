9aeda3ef44d9e38002e36db978d03eb0d1721f50
venkatasaranya <b.saranyareddy25@gmail.com>
2018-07-31 13:38:08 -0400

    first commit

---
+++ src/main/java/group5/App.java
@@ -0,0 +1,31 @@
+package src.main.java.group5;
+
+
+
+
+/**
+ * Hello world!
+ *
+ */
+
+
+
+public class App 
+{
+    
+
+
+public static void main( String[] args )
+    
+
+
+{
+        
+
+System.out.println( "Hello World!" );
+    
+
+}
+
+
+}
